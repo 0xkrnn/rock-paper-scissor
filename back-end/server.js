eebaf043e067b49f4798965cf6ad8e16b95d602d
0xkrnn <karannataraj301@gmail.com>
2024-04-09 18:52:15 +0530

    commit

--- back-end/server.js
+++ back-end/server.js
@@ -8,9 +8,7 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
-app.use(cors({
-    origin: "http://localhost:5173",
-}))
+app.use(cors())
 
 
 app.get("/", (req, res) => {
